0eee2e72330e3ef014c72591903b00d3f1206a67
Renato Schmidt <rescbr@gmail.com>
2024-07-07 20:17:15 -0300

    uses @cactus1549 suggestion to fix #661

--- src/pyfolio/timeseries.py
+++ src/pyfolio/timeseries.py
@@ -891,7 +891,7 @@ def get_max_drawdown_underwater(underwater):
         The maximum drawdown's recovery.
     """
 
-    valley = underwater.index[np.argmin(underwater)]  # end of the period
+    valley = underwater.index[underwater.values.argmin()]  # end of the period
     # Find first 0
     peak = underwater[:valley][underwater[:valley] == 0].index[-1]
     # Find last 0
